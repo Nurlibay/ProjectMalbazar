2bcce4b73af9a1d6a75f260b599bf6fd50c22065
AtabekNazarbaev <atabeknazarbaev441@gmail.com>
2022-01-12 18:01:58 +0500

    letters

--- app/src/main/java/uz/texnopos/malbazar/ui/main/info/comments/CommentsFragment.kt
+++ app/src/main/java/uz/texnopos/malbazar/ui/main/info/comments/CommentsFragment.kt
@@ -2,14 +2,13 @@ package uz.texnopos.malbazar.ui.main.info.comments
 
 import addLetterToWord
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import hideProgress
-import kotlinx.android.synthetic.main.fragment_comments.*
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import showProgress
 import textToString
@@ -17,10 +16,10 @@ import toast
 import uz.texnopos.malbazar.R
 import uz.texnopos.malbazar.core.Constants
 import uz.texnopos.malbazar.core.ResourceState
+import uz.texnopos.malbazar.core.imagehelper.setDrawableImage
 import uz.texnopos.malbazar.core.preferences.isSignedIn
 import uz.texnopos.malbazar.core.preferences.userId
 import uz.texnopos.malbazar.data.model.AddComment
-import uz.texnopos.malbazar.data.model.GetComments
 import uz.texnopos.malbazar.databinding.FragmentCommentsBinding
 
 class CommentsFragment : Fragment(R.layout.fragment_comments) {
@@ -29,6 +28,7 @@ class CommentsFragment : Fragment(R.layout.fragment_comments) {
     private val args: CommentsFragmentArgs by navArgs()
     private val adapter = CommentsAdapter()
     private lateinit var binding: FragmentCommentsBinding
+    private var visible = 1
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -36,6 +36,22 @@ class CommentsFragment : Fragment(R.layout.fragment_comments) {
         viewModel.getComments(args.animalId)
         setUpObservable()
         binding.apply {
+
+            ivLettersVisible.setOnClickListener {
+                if (visible == 0) {
+                    llLetters.isVisible = true
+                    ivLettersVisible.setDrawableImage(R.drawable.ic_baseline_remove_red_eye_24)
+                    visible = 1
+                } else {
+                    llLetters.isVisible = false
+                    ivLettersVisible.setDrawableImage(R.drawable.not_show)
+                    visible = 0
+                }
+            }
+
+            swipe.setOnRefreshListener {
+                viewModel.getComments(args.animalId)
+            }
 
             toolbar.setNavigationOnClickListener {
                 requireActivity().onBackPressed()
@@ -104,21 +120,22 @@ class CommentsFragment : Fragment(R.layout.fragment_comments) {
     }
 
     private fun setUpObservable() {
-        viewModel.getComment.observe(viewLifecycleOwner) {
-            when (it.status) {
-                ResourceState.LOADING -> showProgress()
-                ResourceState.SUCCESS -> {
-                    hideProgress()
-                    adapter.models = it.data!!
-                }
-                ResourceState.ERROR -> {
-                    hideProgress()
-                    it.message?.let { it1 -> Log.d("ER", it1) }
-                    it.message?.let { it1 -> toast(it1) }
-                }
-                ResourceState.NETWORK_ERROR -> {
-                    hideProgress()
-                    toast(Constants.NO_INTERNET)
+        binding.apply {
+            viewModel.getComment.observe(viewLifecycleOwner) {
+                when (it.status) {
+                    ResourceState.LOADING -> swipe.isRefreshing = true
+                    ResourceState.SUCCESS -> {
+                        swipe.isRefreshing = false
+                        adapter.models = it.data!!
+                    }
+                    ResourceState.ERROR -> {
+                        swipe.isRefreshing = false
+                        it.message?.let { it1 -> toast(it1) }
+                    }
+                    ResourceState.NETWORK_ERROR -> {
+                        hideProgress()
+                        toast(Constants.NO_INTERNET)
+                    }
                 }
             }
         }
